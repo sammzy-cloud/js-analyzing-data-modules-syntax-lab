f41c7324c43e8b4c8fb27e00a1721339c1f50290
sammzy-cloud <samanthangiela@gmail.com>
2026-07-02 11:16:16 -0700

    Correcting spelling errors

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+//Write your code here
+require("datejs");
+function combineUsers(...args){
 
+  const combinedObject = {
+    users: []
+  };
+  for(const userArray of args){
+    combinedObject.users.push(...userArray)
+
+  }
+combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+return combinedObject;
+}
 
 
 module.exports = {
